0e294f9453ec95d90f12d199c7d5a7b19bf1c2c8
naoki senda <naoki.senda@qnote.co.jp>
2025-05-14 22:42:15 +0900

    属性バック

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,5 +3,5 @@
     comps
     <x-test.card title="タイトル" content="コンポーネント側で設定した変数をblade側から引数として代入する" :message="$message"></x-test> 
     <x-test.card title="タイトル2"></x-test> 
-
+    <x-test.card title="css変更したい!" class="text-red-500">$attribute忘れずに</x-test.card>
 </x-test.app>
