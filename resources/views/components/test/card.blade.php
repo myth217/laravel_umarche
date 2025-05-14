b8f72f205254463a2132dd1293417cd7edb7c91e
naoki senda <naoki.senda@qnote.co.jp>
2025-05-14 22:32:22 +0900

    propsについて

--- resources/views/components/test/card.blade.php
+++ resources/views/components/test/card.blade.php
@@ -1,3 +1,11 @@
+@props(
+[
+'title' => '初期値なわけ',
+'message' => '初期値ですわよ',
+'content' => 'これも初期値ですわよ'
+
+//propsを一つでも設定する場合種類設定する必要あり
+])
 <div class="border-2 shadow-md w-1/4 p-2">
     <div>{{$title}}</div>
     <div>画像</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,6 @@
     <x-slot name="header">ヘッダー1</x-slot>
     comps
     <x-test.card title="タイトル" content="コンポーネント側で設定した変数をblade側から引数として代入する" :message="$message"></x-test> 
+    <x-test.card title="タイトル2"></x-test> 
 
 </x-test.app>
