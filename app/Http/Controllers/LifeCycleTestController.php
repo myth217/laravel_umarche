b7f0b1b1bba1144ce420cc9eba17471406674c71
naoki senda <naoki.senda@qnote.co.jp>
2025-05-15 00:00:13 +0900

    サービスコンテナ

---
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LifeCycleTestController extends Controller
+{
+    //
+    public function showServiceContainerTest() {
+        app()->bind('lifeCycleTest', function(){
+            return 'ライフサイクルテストなう';
+        });
+
+        $test = app()->make('lifeCycleTest');
+
+        // サービスコンテナなしのパターン
+        // $message = new Message();
+        // $sample = new Sample($message);
+        // $sample->run();
+
+        // サービスコンテナありのパターン
+        app()->bind('sample', Sample::class); //クラスを紐づけられる
+        $sample = app()->make('sample');
+        $sample->run();
+
+        dd($test,app());
+    }
+}
+
+
+class Sample {
+    public $message;
+    public function __construct(Message $message) { //Messageを自動でインスタンス化
+        $this->message = $message; //$this->message の中に　Messageクラスをインスタンス化
+
+    }
+    public function run() {
+        $this->message->send();
+    }
+}
+
+class Message
+{
+    public function send() {
+        echo('めっさ表示');
+    }
+}
+
